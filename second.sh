b1c6ae7283950d6cdfdb4b0628c7b5a22776928a
Aman Singh <amanadhikariaps@gmail.com>
2023-12-05 16:55:03 +0530

    a basic shell script that will move the last minute file from source to destination dir

--- second.sh
+++ second.sh
@@ -1,7 +1,13 @@
 #transfering the files from one location to another
 
-previous_minute=$(date -d ' minute ago' +'%Y%m%d_%H%M')
+previous_minute=$(date -d '1 minute ago' +'%Y%m%d_%H%M')
 
-file_name="$previous_minute.txt"
+file_name="$previous_minute.json"
 
-file_path=x
+file_path="/home/ispock/Desktop/data/aman/"
+
+destination="/home/ispock/Documents/data"
+
+mv "$file_path$file_name" $destination
+
+ls $destination
