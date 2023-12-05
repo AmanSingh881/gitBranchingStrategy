809f26ac7c37a12a3aaf09b7bd84127faf60c9e2
Aman Singh <amanadhikariaps@gmail.com>
2023-12-05 16:29:38 +0530

    python code complete

--- create_data.py
+++ create_data.py
@@ -1,31 +1,60 @@
-# using this program we will create a program which will save date and time in a txt file every second . after every hour a new file will be created.
-import os
+import json,random
 import time
-from datetime import datetime
+import os, pytz
+from datetime import datetime, timedelta
 
-def create_file():
-    current_time = datetime.now()
-    file_name = f"data_{current_time.strftime('%Y%m%d_%H%M')}.txt"
+DATA_DIR = "/home/ispock/Desktop/data/"  
+ist =  pytz.timezone('Asia/Kolkata')
+def create_new_file(key):
+    ist_offset = timedelta(hours=5, minutes=30)
+    utc_time = datetime.utcnow()
+    ist_time = utc_time + ist_offset
+    file_name = str(ist_time.strftime("%Y-%m-%d %H%M")) 
+    file_path = os.path.join(key, file_name)
+    return file_path
+
+
+def append_data_to_json_file(data, file_path):
+    try:
+        with open(file_path+'.json', "a") as file:
+            json.dump(data, file)
+            file.write("\n")
+    except Exception as e:
+        print(f"2 Error occurred in appending data in the file : {e}")
+
+
+
+def start_server():
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
     
-    if not os.path.exists(file_name):
-        with open(file_name, 'a') as file:
-            file.write(f"File created at: {current_time}\n")
-
-    return file_name
-
-def append_random_data(file_name):
-    with open(file_name, 'a') as file:
-        current_time = datetime.now()
-        file.write(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
-
-def main():
+    print("Server started. Waiting for clients...")
+    
     while True:
-        current_minute = datetime.now().minute
-        file_name = create_file()
+            current_minute = None
+            current_file = None
+            
+            while True:
+                try:       
+                    data_dict = {"aman":[int(time.time()*1000),random.randint(1,100)]*10}
+                    timestamp = datetime.now(ist)
+                    
+                    for key in data_dict:
+                        pass
 
-        while datetime.now().minute == current_minute:
-            append_random_data(file_name)
-            time.sleep(1)
+                    path1 = DATA_DIR+key
+                    if not os.path.exists(path1):
+                        os.makedirs(path1)
+
+                    if current_minute != timestamp.minute:
+                        current_minute = timestamp.minute
+                        current_file = create_new_file(path1)
+                    print(data_dict)
+                    append_data_to_json_file(data_dict, current_file)
+
+                except Exception as e:
+                    print(f"An exception occured : {e}, {time.time()}")
+                time.sleep(1)
 
 if __name__ == "__main__":
-    main()
+    start_server()
